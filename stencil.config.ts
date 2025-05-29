2f1b5bc84cc0c66d29a5d2efa4bf9c69a3bdc212
notHyotic <hy0t1c1@gmail.com>
2025-05-28 21:54:15 -0500

    add secret

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'common-ui-components',
+  namespace: 'common-web-components',
   outputTargets: [
     {
       type: 'dist',
